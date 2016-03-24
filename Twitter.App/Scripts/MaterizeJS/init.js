748112433df43bad609a08f7de056723c8aea66f
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-03-24 23:56:05 +0800

    add toast to about.cshtml and dev.cshtml

--- Twitter.App/Scripts/MaterizeJS/init.js
+++ Twitter.App/Scripts/MaterizeJS/init.js
@@ -6,3 +6,10 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function () {
+    $('.slider').slider({ full_width: true });
+});
+
+// Materialize.toast(message, displayLength, className, completeCallback);
+Materialize.toast('Welcome to QQmgs.com', 4000); // 4000 is the duration of the toast
