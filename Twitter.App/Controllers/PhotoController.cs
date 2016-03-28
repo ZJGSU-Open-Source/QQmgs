284d219aeb94151f705989dfff6f8b50468468bd
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-03-28 22:20:56 +0800

    add photo controller

---
+++ Twitter.App/Controllers/PhotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Twitter.App.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // GET: Photo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
