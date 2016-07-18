a2d1352c3e5f0bca67cf3c16ebfc6077190c3e9a
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-07-18 21:59:15 +0800

    add conversation controller

---
+++ Twitter.App/BusinessLogic/Base64Security.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Glimpse.Core.Extensibility;
+using Twitter.App.Common;
+
+namespace Twitter.App.BusinessLogic
+{
+    public static class Base64Security
+    {
+        public static string Encrption(string uid1, string uid2)
+        {
+            Guard.ArgumentNotNullOrEmpty(uid1, nameof(uid1));
+            Guard.ArgumentNotNullOrEmpty(uid2, nameof(uid2));
+
+            var stringValue = uid1 + "_" + uid2;
+            var bytes = Encoding.UTF8.GetBytes(stringValue);
+            var cypherString = Convert.ToBase64String(bytes);
+
+            return cypherString;
+        }
+
+        public static string Decrption(string cid)
+        {
+            Guard.ArgumentNotNullOrEmpty(cid, nameof(cid));
+
+            var bytes = Convert.FromBase64String(cid);
+            var users = Encoding.UTF8.GetString(bytes);
+
+            return users;
+        }
+    }
+}
---
+++ Twitter.App/Controllers/MVCController/ConversationController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Twitter.App.BusinessLogic;
+using Twitter.App.Common;
+
+namespace Twitter.App.Controllers.MVCController
+{
+    [Authorize]
+    [RoutePrefix("conversation")]
+    public class ConversationController : Controller
+    {
+        // GET: Chatting
+        public ActionResult Chatting(string cid)
+        {
+            var userPair = Base64Security.Decrption(cid);
+            var users = userPair.Split('_');
+
+            Guard.Argument(() => users.Length == 2, nameof(users));
+
+            var sender = users[0];
+            var receiver = users[1];
+
+            Guard.ArgumentNotNullOrEmpty(sender, nameof(sender));
+            Guard.ArgumentNotNullOrEmpty(receiver, nameof(receiver));
+
+            ViewData["sender"] = sender;
+            ViewData["receiver"] = receiver;
+
+            return View();
+        }
+
+        // GET: Chatting
+        public ActionResult GenerateChatting(string user1, string user2)
+        {
+            var cypherString = Base64Security.Encrption(user1, user2);
+            ViewData["string"] = cypherString;
+
+            return View();
+        }
+
+        // GET: Conversation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Conversation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Conversation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Conversation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conversation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Conversation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conversation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Conversation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
