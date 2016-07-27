47374a7e4398610274a9c0d7732b852f405ac609
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-07-27 23:53:47 +0800

    BIG CHANGE update css, js and font, add slides on group

--- Twitter.App/Scripts/MaterizeJS/init.js
+++ Twitter.App/Scripts/MaterizeJS/init.js
@@ -18,9 +18,3 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.scrollspy').scrollSpy();
 });
-
-// load modal
-$(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-});
