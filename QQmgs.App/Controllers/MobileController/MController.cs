67bc7ff14b913b414b13f138c0b3dbef09c519cf
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-10-19 15:05:52 +0800

    add mobile routing

---
+++ QQmgs.App/Controllers/MobileController/MController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Twitter.App.Controllers.MobileController
+{
+    [RoutePrefix("m")]
+    public class MController : Controller
+    {
+        // GET: M
+        [Route("")]
+        public ActionResult Redirect()
+        {
+            return RedirectToAction("Activity");
+        }
+
+        // GET: Activity
+        [Route("activity")]
+        public ActionResult Activity()
+        {
+            return View();
+        }
+    }
+}
