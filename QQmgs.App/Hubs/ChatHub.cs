42bb6908bc0347be6905ff7fd344c036b3ea30aa
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-12-23 22:16:54 +0800

    add more friendly notification for chatting

--- QQmgs.App/Hubs/ChatHub.cs
+++ QQmgs.App/Hubs/ChatHub.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -8,10 +10,25 @@ namespace Twitter.App.Hubs
 {
     public class ChatHub : Hub
     {
+        private static int _userNumber = 0;
+
         public void Send(string name, string message)
         {
-            // Call the addNewMessageToPage method to update clients.
             Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            ++_userNumber;
+
+            return null;
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Clients.All.addNewMessageToPage("【系统消息】", $"有一位小伙伴离开了群聊, {_userNumber--} 人当前在线.");
+
+            return null;
         }
     }
 }
