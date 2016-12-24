068d0ec7da3ff21976e9d725b505e6040c1cef33
Shuaiyi Wu <wushuaiyi@outlook.com>
2016-12-24 15:12:27 +0800

    include js files and minior update layouts

--- QQmgs.App/Hubs/ChatHub.cs
+++ QQmgs.App/Hubs/ChatHub.cs
@@ -2,6 +2,8 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
@@ -10,13 +12,54 @@ namespace Twitter.App.Hubs
 {
     public class ChatHub : Hub
     {
+        // Current online user number
         private static int _userNumber = 0;
+
+        private static readonly ConcurrentDictionary<string, ChatUser> Users = new ConcurrentDictionary<string, ChatUser>(StringComparer.OrdinalIgnoreCase);
+
+        public static string DefaultChattingRoom { get; } = "defaultRoom";
+
+        public bool Join()
+        {
+            return false;
+
+            // Check the user id cookie
+            Cookie userIdCookie;
+
+            if (!Context.RequestCookies.TryGetValue("QQmgs-chat-userid", out userIdCookie))
+            {
+                return false;
+            }
+
+            var user = Users.Values.FirstOrDefault(u => u.Id == userIdCookie.Value);
+
+            if (user == null)
+            {
+                // add new user and add to client caller
+                AddUser(new DateTime().ToLongTimeString());
+
+                return false;
+            }
+
+            // Update the users's client id mapping if already connected
+            user.ConnectionId = Context.ConnectionId;
+
+            // Set some client state
+            Clients.Caller.id = user.Id;
+            Clients.Caller.name = user.Name;
+            Clients.Caller.hash = user.Hash;
+                
+            // Add this user to the list of users
+            Clients.Caller.addUser(user);
+
+            return true;
+        }
 
         public void Send(string name, string message)
         {
             Clients.All.addNewMessageToPage(name, message);
         }
-
+        
         public override Task OnConnected()
         {
             ++_userNumber;
@@ -28,7 +71,93 @@ namespace Twitter.App.Hubs
         {
             Clients.All.addNewMessageToPage("【系统消息】", $"有一位小伙伴离开了群聊, {_userNumber--} 人当前在线.");
 
+            var user = Users.Values.FirstOrDefault(u => u.ConnectionId == Context.ConnectionId);
+
+            if (user != null)
+            {
+                ChatUser ignoredUser;
+                Users.TryRemove(user.Name, out ignoredUser);
+            }
+
             return null;
+        }
+
+        public IEnumerable<ChatUser> GetUsers()
+        {
+            var users = Users.Select(user => new ChatUser
+            {
+                Id = user.Value.Id,
+                Name = user.Value.Name,
+                ConnectionId = user.Value.ConnectionId,
+                Hash = user.Value.Hash
+            });
+
+            return users;
+        }
+
+        private static string GetMD5Hash(string name)
+        {
+            return string.Join("", MD5.Create()
+                .ComputeHash(Encoding.Default.GetBytes(name))
+                .Select(b => b.ToString("x2")));
+        }
+
+        private ChatUser AddUser(string newUserName)
+        {
+            var user = new ChatUser(newUserName, GetMD5Hash(newUserName))
+            {
+                ConnectionId = Context.ConnectionId
+            };
+
+            Users[newUserName] = user;
+
+            Clients.Caller.name = user.Name;
+            Clients.Caller.hash = user.Hash;
+            Clients.Caller.id = user.Id;
+
+            Clients.Caller.addUser(user);
+
+            return user;
+        }
+    }
+
+    [Serializable]
+    public class ChatMessage
+    {
+        public string Id { get; private set; }
+
+        public string User { get; set; }
+
+        public string Text { get; set; }
+
+        public ChatMessage(string user, string text)
+        {
+            User = user;
+            Text = text;
+            Id = Guid.NewGuid().ToString("d");
+        }
+    }
+
+    [Serializable]
+    public class ChatUser
+    {
+        public string ConnectionId { get; set; }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Hash { get; set; }
+
+        public ChatUser()
+        {
+        }
+
+        public ChatUser(string name, string hash)
+        {
+            Name = name;
+            Hash = hash;
+            Id = Guid.NewGuid().ToString("d");
         }
     }
 }
---
+++ QQmgs.App/Scripts/Chat/Chatting.js
@@ -0,0 +1,89 @@
+﻿$(function () {
+    // clear mobile footer
+    $("#mobile-footer").remove();
+
+    // Reference the auto-generated proxy for the hub.
+    var chat = $.connection.chatHub;
+
+    function makeUserName() {
+        var nameList = [
+            "钱江湾", "金沙港", "金字塔", "字母楼", "保研路", "酱饼妹", "裸奔男", "墨湖", "碧湖", "教工路", "学正街", "二号大街", "中门", "球门", "鸟门",
+            "信息楼", "经济楼", "管理楼", "食品楼", "环境楼", "二号田径场", "钱塘江", "金沙湖"
+        ];
+
+        var idx = Math.floor(Math.random() * (nameList.length - 1));
+        return nameList[idx];
+    }
+
+    $("#message").keyup(function (event) {
+        if (event.keyCode === 13) {
+            $("#sendmessage").click();
+        }
+    });
+
+    function updateCookie() {
+        $.cookie('QQmgs-chat-userid', chat.state.id, { path: '/', expires: 30 });
+    }
+
+    // Auto generate name for user
+    var userName = makeUserName();
+    Materialize.toast('随机分配得到你的匿名昵称: ' + userName, 18000);
+
+    // Get the user name and store it to prepend to messages.
+    $('#displayname').val(userName);
+
+    $('#title').append('<h6>当前昵称: ' + userName + '. 刷新页面更换新昵称.</h6>');
+
+    // Create a function that the hub can call back to display messages.
+    chat.client.addNewMessageToPage = function (name, message) {
+
+        // Get current time
+        var currentdate = new Date().toLocaleTimeString('en-US',
+        {
+            hour12: false,
+            hour: "numeric",
+            minute: "numeric"
+        });
+
+        if (name === '【系统消息】') {
+            // Add the message to the page.
+            $('#discussion').append('<li class="collection-item" style="color: cornflowerblue; font-size: 12px; padding-top: 2px; padding-bottom: 1px; padding-left: 15px;">' + currentdate + ' <strong>' + htmlEncode(name)
+                + '</strong>: ' + htmlEncode(message) + '</li>');
+        } else {
+            // Add the message to the page.
+            $('#discussion').append('<li class="collection-item" style="padding-left: 15px">' + currentdate + ' <strong>' + htmlEncode(name)
+                + '</strong>: ' + htmlEncode(message) + '</li>');
+        }
+
+        // Scroll to page bottom
+        $("#discussion").scrollTop($("#discussion")[0].scrollHeight);
+    };
+
+    // Set initial focus to message input box.
+    $('#message').focus();
+
+    // Start the connection.
+    $.connection.hub.start({ transport: window.activeTransport },
+        function () {
+            chat.server.join()
+                .done(function () {
+                    chat.server.send('【系统消息】', '欢迎“' + userName + '”加入群聊.');
+
+                    $('#sendmessage')
+                        .click(function () {
+
+                            // Call the Send method on the hub.
+                            chat.server.send(userName, $('#message').val());
+
+                            // Clear text box and reset focus for next comment.
+                            $('#message').val('').focus();
+                        });
+                });
+        });
+
+    // This optional function html-encodes messages for display in the page.
+    function htmlEncode(value) {
+        var encodedValue = $('<div />').text(value).html();
+        return encodedValue;
+    }
+})
